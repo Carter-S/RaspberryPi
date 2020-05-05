17d6885ce41778c0dc63232c37c1fa69ce72583f
Carter-S <carter.springall01@gmail.com>
2020-05-05 21:50:32 +0100

    lcd display added

---
+++ lcdTesting/lcdTest.py
@@ -0,0 +1,14 @@
+import lcddriver
+import time
+import datetime
+
+display = lcddriver.lcd()
+
+try:
+    print("Writing to display")
+    display.lcd_display_string("No time to waste", 1)
+    while True:
+        display.lcd_display_string(str(datetime.datetime.now().time()), 2)
+finally:
+    print("Cleaning up!")
+    display.lcd_clear()
